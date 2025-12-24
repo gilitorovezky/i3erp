6fd30c8783d49b9090cf1a2f77141d16f34f00d6
gtorovezky <gilitorovezky@gmail.com>
2025-12-23 18:24:29 -0800

    rwmove the button from the files element
    Signed-off-by: gtorovezky <gilitorovezky@gmail.com>

--- fileops/fileops.js
+++ fileops/fileops.js
@@ -189,7 +189,9 @@ function uploadFilesMngr(isFiles,isPrjSet,ID) {  // 1st parameter is a flag that
     var fileupload="";
 
     if ( !isFiles )
-        fileupload= `<button tabindex="0" id="allFilesID" type="button" class="button2">Upload file(s)</button>`; //disable the upload if project is unset
+        //fileupload= `<button tabindex="0" id="allFilesID" type="button" class="button2">Upload file(s)</button>`; //disable the upload if project is unset
+        fileupload= `Upload file(s)`; //disable the upload if project is unset
+
     else    
         fileupload='<select tabindex="0" id="selectFiles" class="filesSelect button-view"><option value="uploadFiles">Upload Files</option><option value="showFiles">Show Files</option></select>';
 
